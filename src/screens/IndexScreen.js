5ff5ad3e633ed9270b3d23accd652387dfa5e38b
KainoaKeanaaina <kaikeanaaina@gmail.com>
2019-11-25 15:53:01 -1000

    added button, able to use addBlogPost

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -4,17 +4,18 @@ import React, { useContext } from 'react'
 // give us access to things' values props
 // whenever we want to pass down information
 // useContext is what we use
-import { View, Text, StyleSheet, FlatList } from 'react-native'
+import { View, Text, StyleSheet, FlatList, Button } from 'react-native'
 import BlogContext from '../context/BlogContext'
 
 const IndexScreen = () => {
-    const blogPosts = useContext(BlogContext);
+    const {data, addBlogPost} = useContext(BlogContext);
 
     return (
         <View>
             <Text>Index Screen</Text>
+            <Button title="Add Post" onPress={addBlogPost} />
             <FlatList 
-                data={blogPosts}
+                data={data}
                 keyExtractor={blogPost => blogPost.title}
                 renderItem={({item}) => {
                     return <Text>{item.title}</Text>
