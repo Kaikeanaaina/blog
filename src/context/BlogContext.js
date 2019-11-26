ce6555f9a54d739d0b401ce8e631a8b355d7ed6f
KainoaKeanaaina <kaikeanaaina@gmail.com>
2019-11-25 18:37:02 -1000

    reducer and dispatch is set up

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,8 +1,17 @@
-import React, {useState} from 'react';
+import React, { useReducer } from 'react';
 
 const BlogContext = React.createContext();
 // a pipe, an object responsible for giving information 
 // from blog post provider to any children
+
+const blogReducer = (state, action) => {
+    switch (action.type) {
+        case 'add_blogpost':
+            return [...state, { title: `Blog Post #${state.length + 1}`}]
+        default:
+            return state
+    }
+}
 
 export const BlogProvider = ({children}) => {
     //this is the blog provider from the toptop level
@@ -12,10 +21,11 @@ export const BlogProvider = ({children}) => {
     // 'return <BlogContext.Provider>{children}</BlogContext.Provider>'
     // can't pass down an value object yet
 
-    const [blogPosts, setBlogPosts] = useState([])
+    const [blogPosts, dispatch] = useReducer(blogReducer, [])
+    //blogPosts can be seen as state
 
     const addBlogPost = () => {
-        setBlogPosts([ ...blogPosts, { title: `Blog Post #${blogPosts.length + 1}`}]);
+        dispatch({type:'add_blogpost'})
     }
 
     // the value layout is how you pass data and functions down to the children
