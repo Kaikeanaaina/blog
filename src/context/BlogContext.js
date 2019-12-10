9c86e9986953230234e4712a9636acee0c5ae872
KainoaKeanaaina <kaikeanaaina@gmail.com>
2019-12-10 00:20:11 -1000

    notes

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,11 +1,16 @@
+//1
 import React, { useReducer } from 'react';
 
+//2
 const BlogContext = React.createContext();
 // a pipe, an object responsible for giving information 
 // from blog post provider to any children
 
+//5
+// this is my reducer function
 const blogReducer = (state, action) => {
     switch (action.type) {
+        // 8
         case 'add_blogpost':
             return [...state, { title: `Blog Post #${state.length + 1}`}]
         default:
@@ -13,6 +18,7 @@ const blogReducer = (state, action) => {
     }
 }
 
+    //3
 export const BlogProvider = ({children}) => {
     //this is the blog provider from the toptop level
     // we create an component, that will take another component as an argument
@@ -21,13 +27,22 @@ export const BlogProvider = ({children}) => {
     // 'return <BlogContext.Provider>{children}</BlogContext.Provider>'
     // can't pass down an value object yet
 
+    //6 - needs a state, a reducer, and a dispatch
     const [blogPosts, dispatch] = useReducer(blogReducer, [])
-    //blogPosts can be seen as state
+    //blogPosts can be seen as "state"
+    //dispatch is like fire up this function
+    // the first argumment "blogReducer" is the reducer we use
+    // the second argument "[]" will be our initial state object
+    // // by default we don't have any blog at all
 
+    // 7
     const addBlogPost = () => {
         dispatch({type:'add_blogpost'})
     }
+    // this fires up the function
+    // goes into the reducer and does the reducer actions
 
+    //4
     // the value layout is how you pass data and functions down to the children
     // {data: blogPosts, addBlogPost: addBlogPost}
     return (<BlogContext.Provider value={{data: blogPosts, addBlogPost }}>
@@ -35,4 +50,5 @@ export const BlogProvider = ({children}) => {
     </BlogContext.Provider>)
 }
 
+//2.5
 export default BlogContext;
