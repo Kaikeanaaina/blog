d5aa7369663463b9ca9c28e60116b3b96445abc7
KainoaKeanaaina <kaikeanaaina@gmail.com>
2019-12-10 01:34:29 -1000

    changed import to Provider, and the components in the return

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import IndexScreen from './src/screens/IndexScreen'
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import { BlogProvider } from './src/context/BlogContext'
+import { Provider } from './src/context/BlogContext'
 
 const navigator = createStackNavigator(
   {
@@ -21,7 +21,7 @@ const App = createAppContainer(navigator)
 export default () => {
   // this allows us to customize our component
   // instead of the usual 'export default createAppContainer(navigator)  
-  return <BlogProvider>
+  return <Provider>
     <App />
-  </BlogProvider>
+  </Provider>
 }
