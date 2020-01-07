8f4c163be4d03a4f317df201559c0d58dcfd5ece
KAINOA KEANAAINA <kainoakeanaaina@HI201-kshultz.localdomain>
2020-01-07 12:03:23 -1000

    imported ShowScreen.js

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import IndexScreen from './src/screens/IndexScreen'
+import ShowScreen from './src/screens/ShowScreen'
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { Provider } from './src/context/BlogContext'
 
 const navigator = createStackNavigator(
   {
-    Index: IndexScreen
+    Index: IndexScreen,
+    Show: ShowScreen
   },
   {
     initialRouteName: 'Index',
