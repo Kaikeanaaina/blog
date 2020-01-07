e9d33465ec93792bd46615906c707b69b327ac57
KAINOA KEANAAINA <kainoakeanaaina@HI201-kshultz.localdomain>
2020-01-07 11:34:21 -1000

    now each blogpost has an id when created

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -10,19 +10,25 @@ const blogReducer = (state, action) => {
         // 8
         case 'add_blogpost':
             return [
-                ...state, 
-                { 
-                    id: Math.floor(Math.random() * 99999), 
-                    title: `Blog Post #${state.length + 1}` 
+                ...state,
+                {
+                    id: Math.floor(Math.random() * 99999),
+                    title: `Blog Post #${state.length + 1}`
                 }
             ]
+        case 'delete_blogpost':
+            return state.filter((blogPost) => blogPost.id !== action.payload);
+            //the filter still create a new array,
+            // filter will go through the old arra to check if the id matches
+            // if false, it removes the object
+            // then returns a brand new state array
         default:
             return state
     }
 }
 
 // 4 - helper function
-const addBlogPost = (dispatch) => {
+const addBlogPost = dispatch => {
     return () => {
         dispatch({ type: 'add_blogpost' })
     }
@@ -30,12 +36,18 @@ const addBlogPost = (dispatch) => {
 // this fires up the function
 // goes into the reducer and does the reducer actions
 
+const deleteBlogPost = dispatch => {
+    return (id) => {
+        dispatch({ type: 'delete_blogpost', payload: id })
+    };
+}
+
 //5 - creates the Context and Provider
 // 1st argument - reducer used
 // 2nd argument - helper functions, where we add all the different functions we want
 // 3rd argument - initial state devalue
 export const { Context, Provider } = createDataContext(
     blogReducer,
-    { addBlogPost },
+    { addBlogPost, deleteBlogPost },
     []
 )
